125d98238d5f622642d8b35e185577eedba598a1
Devika Bakshi <devika.bakshi2004@gmail.com>
2024-05-04 13:58:45 +0530

    seventh

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.set('views', __dirname + "/src/views");
+app.set('views', path.join(__dirname, 'src/views'));
+
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
-  res.render("pages/home");
+  res.send("pages/home");
 });
+
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
